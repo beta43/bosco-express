b0f3be9664551127f51d1f850b70151672594e67
cleverGek <kg.geku@gmail.com>
2022-03-07 13:43:02 +0100

    login and signup

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Octal" });
 });
 
+/* GET home page. */
+router.get("/login", function (req, res, next) {
+  res.render("./pages/login", { title: "Octal" });
+});
+
+/* GET home page. */
+router.get("/sign-up", function (req, res, next) {
+  res.render("./pages/signup", { title: "Octal" });
+});
+
+/* GET home page. */
+router.get("/sign-up-prestator", function (req, res, next) {
+  res.render("./pages/signup-prestator", { title: "Octal" });
+});
+
 module.exports = router;
